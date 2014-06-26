e5f09dd7e9a9d847431bafa9681aff1f1a7388c7
Tyler Garrett <jtgarrett123@gmail.com>
2014-06-26 11:26:39 -0700

    fix(update & uninstall): made code that was used in both methods into a function

--- index.js
+++ index.js
@@ -3,68 +3,77 @@
 var fs = require('fs');
 
 // read from json file, and pass it into callback function as 2nd argument
-// read(err, function (err, manifest) {
+// read(filepath, function (err, manifest) {
 //   manifest.name = 'packageName'
 // })
 
 var read = exports.read = require('read-json');
 
+// Takes file path for external-scripts and package, and callback
+// then calls callback with returned data in an obj
+var getFileData = function(externalScriptsFile, packageJsonFile, callback) {
+  var fileData = {};
+  read(externalScriptsFile, function(err, data) {
+    if (err) {
+      callback(err);
+    }
+  fileData.externalScripts = data;
+    read(packageJsonFile, function(err, data) {
+      if (err) {
+        callback(err);
+      }
+      fileData.packageJson = data;
+      if(callback && typeof callback === "function") {
+        callback(undefined, fileData);
+      }
+    });
+  });
+};
 
 // should take path to external-scripts, path to package.json, optional callback
 // it directly edits external scripts file with package.json["name"]
-var update = exports.update = function(externalScriptsFile, fileWithName, callback) {
-  read(externalScriptsFile, function(err, data) {
-
-    var externalScripts = data;
-    read(fileWithName, function(err, data) {
-
-      var packageJSON = data;
-      externalScripts.push(packageJSON.name);
-      externalScripts = JSON.stringify(externalScripts);
-
-      fs.writeFile(externalScriptsFile, externalScripts, function(err) {
-        if(err) {
-            console.log(err);
-        }
-        if(callback && typeof callback === "function") {
-          callback();
-        }
-      });
+var update = exports.update = function(externalScriptsFile, packageJsonFile, callback) {
+  getFileData(externalScriptsFile, packageJsonFile, function(err, fileData) {
+    if (err) {
+      return callback(err);
+    }
+    if (fileData.externalScripts.indexOf(fileData.packageJson.name) !== -1) {
+      return callback(err);
+    }
+    fileData.externalScripts.push(fileData.packageJson.name);
+    var externalScriptsStr = JSON.stringify(fileData.externalScripts);
+    fs.writeFile(externalScriptsFile, externalScriptsStr, function(err) {
+      if(err) {
+          return callback(err);
+      }
+      if(callback && typeof callback === "function") {
+        callback();
+      }
     });
-  });
+  })   
 };
-
 
 // should take path to external-scripts, path to package.json, optional callback
 // it directly edit external scripts file with removal of name from package.json["name"]
-var uninstall = exports.uninstall = function(externalScriptsFile, fileWithName, callback) {
-  read(externalScriptsFile, function(err, data) {
-    var externalScripts = data;
-    read(fileWithName, function(err, data) {
-      var packageJSON = data;
-      var toSplice = externalScripts.indexOf(packageJSON.name);
-      if (toSplice == -1) {
-        if(callback && typeof callback === "function") {
-          return callback(err);
-        } else {
-          return err
-        }
-      } 
-      externalScripts.splice(toSplice, 1);
-      externalScripts = JSON.stringify(externalScripts);
+var uninstall = exports.uninstall = function(externalScriptsFile, packageJsonFile, callback) {
+  getFileData(externalScriptsFile, packageJsonFile, function(err, fileData) {
+    if (err) {
+      return callback(err);
+    }
+    var toSplice = fileData.externalScripts.indexOf(fileData.packageJson.name);
+    if (toSplice === -1) {
+        return callback(err);
+    } 
+    fileData.externalScripts.splice(toSplice, 1);
+    var externalScriptsStr = JSON.stringify(fileData.externalScripts);
 
-      fs.writeFile(externalScriptsFile, externalScripts, function(err) {
-        if(err) {
-            console.log(err);
-        }
-        if(callback && typeof callback === "function") {
-          callback();
-        }
-      });
+    fs.writeFile(externalScriptsFile, externalScriptsStr, function(err) {
+      if(err) {
+          return callback(err);
+      }
+      if(callback && typeof callback === "function") {
+        callback();
+      }
     });
   });
 };
-
-
-
-// uninstall('./test/fixtures/external-scripts.json', './test/fixtures/package.json');
